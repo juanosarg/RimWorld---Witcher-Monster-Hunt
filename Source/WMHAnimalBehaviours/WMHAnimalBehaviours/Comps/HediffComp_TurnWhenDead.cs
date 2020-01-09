99a318cfd23f72eb4ce8bd26abde8cf6bc8c6ec5
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-01-09 16:53:32 +0100

    v0.1

--- Source/WMHAnimalBehaviours/WMHAnimalBehaviours/Comps/HediffComp_TurnWhenDead.cs
+++ Source/WMHAnimalBehaviours/WMHAnimalBehaviours/Comps/HediffComp_TurnWhenDead.cs
@@ -1,13 +1,13 @@
 ﻿using RimWorld;
 using Verse;
+using Verse.Sound;
+using System;
 
 namespace WMHAnimalBehaviours
 {
     public class HediffComp_TurnWhenDead : HediffComp
     {
-        Map map;
-        IntVec3 position;
-
+       
 
         public HediffCompProperties_TurnWhenDead Props
         {
@@ -19,11 +19,29 @@ namespace WMHAnimalBehaviours
 
         public override void Notify_PawnDied()
         {
-            base.Notify_PawnDied();
-            PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.thingToTurnTo), null, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true, false, 1f, false, true, true, false, false);
-            Pawn pawn = PawnGenerator.GeneratePawn(request);
-            PawnUtility.TrySpawnHatchedOrBornPawn(pawn, this.parent.pawn.Corpse);
-            
+            //base.Notify_PawnDied();
+            if (this.parent.pawn.Corpse.Map != null) {
+               
+                PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.thingToTurnTo), Find.FactionManager.FirstFactionOfDef(FactionDef.Named("WMH_Monsters")), PawnGenerationContext.NonPlayer, -1, false, true, false, false, true, false, 1f, false, true, true, false, false);
+                Pawn pawn = PawnGenerator.GeneratePawn(request);
+                PawnUtility.TrySpawnHatchedOrBornPawn(pawn, this.parent.pawn.Corpse);
+                pawn.mindState.mentalStateHandler.TryStartMentalState(DefDatabase<MentalStateDef>.GetNamed("ManhunterPermanent", true), null, true, false, null, false);
+
+                for (int i = 0; i < 20; i++)
+                {
+                    IntVec3 c;
+                    CellFinder.TryFindRandomReachableCellNear(this.parent.pawn.Corpse.Position, this.parent.pawn.Corpse.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                   
+                    FilthMaker.MakeFilth(c, this.parent.pawn.Corpse.Map, ThingDefOf.Filth_Blood);
+                    
+                }
+                
+               
+                SoundDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(this.parent.pawn.Corpse.Position, this.parent.pawn.Corpse.Map, false));
+                this.parent.pawn.Corpse.Destroy();
+
+            }
+
         }
 
        
